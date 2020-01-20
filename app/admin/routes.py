888a4d53611873c4e4d5aa19be441710ff02e0de
Chris Mumford <cmumford@cmumford.com>
2020-01-20 10:57:53 -0800

    Added org users.

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -17,9 +17,27 @@ def organizations():
 @admin_blueprint.route('/organization/<organization_id>', strict_slashes=False)
 def organization(organization_id):
     org = Organization.query.filter(Organization.id == organization_id).first()
-
     user_count = OrganizationRole.query.filter(OrganizationRole.organization_id
             == organization_id).count()
     return render_template('admin/organization.html',
             title=org.name,
+            organization_id=organization_id,
             user_count=user_count)
+
+@admin_blueprint.route('/organization/<organization_id>/<what>',
+        strict_slashes=False)
+def organization_users(organization_id, what):
+    org = Organization.query.filter(Organization.id == organization_id).first()
+    if what == 'users':
+        users = User.query.join(
+                User.organization_roles).filter_by(
+                        organization_id=org.id).limit(40)
+        return render_template('admin/organization_users.html',
+                title='{} Users'.format(org.name),
+                users=users)
+
+@admin_blueprint.route('/user/<user_id>', strict_slashes=False)
+def user(user_id):
+    user = User.query.filter(User.id == user_id).first()
+    return render_template('admin/user.html',
+            title=user.name)
--- app/tests/functional/test_admin.py
+++ app/tests/functional/test_admin.py
@@ -4,3 +4,25 @@ def test_organizations(test_client, init_database):
     assert response.status_code == 200
     assert response.data.decode('utf-8')
     assert b"Summerville" in response.data
+
+def test_organization(test_client, init_database):
+    response = test_client.get('/organization/1')
+    assert response.status_code == 200
+    assert response.data.decode('utf-8')
+    assert b"Gunn High School" in response.data
+
+def test_organization_users(test_client, init_database):
+    response = test_client.get('/organization/1/users')
+    assert response.status_code == 200
+    assert response.data.decode('utf-8')
+    assert b"Gunn High School" in response.data
+    assert b"Barbara Jones" in response.data
+    assert b"Richard Clark" in response.data
+    assert b"Joe Smith" in response.data
+    assert not b"Sarah Parker" in response.data
+
+def test_user(test_client, init_database):
+    response = test_client.get('/user/2')
+    assert response.status_code == 200
+    assert response.data.decode('utf-8')
+    assert b"Richard Clark" in response.data
